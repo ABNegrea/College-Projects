413ce65b92eefcdfdc0f6bd261852a134e4ae46a
ABNegrea <91516893+ABNegrea@users.noreply.github.com>
2023-03-28 11:16:42 +0300

    Add files via upload

---
+++ Children-Competition-C#/Login.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Concurs.Domain;
+using Concurs.Repository;
+using System.Configuration;
+using Concurs.Service;
+using Concurs;
+
+namespace Problema5
+{
+    public partial class Login : Form
+    {
+
+        UserService UserService;
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        static string GetConnectionStringByName(string name)
+        {
+            string returnValue = null;
+            ConnectionStringSettings settings = ConfigurationManager.ConnectionStrings[name];
+            if (settings != null)
+                returnValue = settings.ConnectionString;
+            return returnValue;
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+            UserDBRepository Userrepo = new UserDBRepository(GetConnectionStringByName("P5DB"));
+            UserService = new UserService(Userrepo);
+            label4.Visible = false;
+            textBox2.UseSystemPasswordChar = true;
+            textBox2.PasswordChar = '*';
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            User user = UserService.FindByEmailPassword(textBox1.Text, textBox2.Text);
+            UserDBRepository userDBRepository = new UserDBRepository(GetConnectionStringByName("P5DB"));
+            userDBRepository.Save(new User("andrei", "andrei", "andrei", "andrei", 1));
+            if(user != null)
+            {
+                ConnectedUser connectedUser = new ConnectedUser(user.Id);
+                connectedUser.Show();
+                this.Hide();
+            }
+            else
+            {
+                label4.Visible = true;
+                textBox1.Text = "";
+                textBox2.Text = "";
+            }
+        }
+    }
+}
---
+++ Children-Competition-C#/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Problema5
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Uri configUri = new Uri("D:\\Folder Facultate\\Anul 2 Semestrul 2\\Medii de Proiectate si Programare\\Proiecte-MPP\\Proiect-MPP-C#\\mpp-proiect-csharp-ABNegrea\\Problema5\\log4net.config");
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Login());
+        }
+    }
+}
